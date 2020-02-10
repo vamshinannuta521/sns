1607f3967a42835a006acc6acbecfa986414400d
vamshi.nannuta <vamshi.nannuta@nutanix.com>
2020-02-10 18:07:58 +0530

    WIP

--- service/account/account.go
+++ service/account/account.go
@@ -1,11 +1,46 @@
 package account
 
+import (
+	"sns/dataaccess"
+
+	"github.com/sirupsen/logrus"
+)
+
+var log = logrus.NewEntry(logrus.New())
+
 type SvcInterface interface {
+	Get(string) Account
+	Create([]byte) error
 }
 
 type Svc struct {
 }
 
+type Account struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 func NewSvc() *Svc {
 	return &Svc{}
+}
+
+func (s *Svc) Create(payload []byte) error {
+	var account Account
+	err := json.Unmarshal(payload, &account)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+}
+
+func (s *Svc) Get(accountID string) error {
+	var account Account
+	err := json.Unmarshal(payload, &account)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
 }
