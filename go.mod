60b2777ff2fec837ef95d607b7e8e1e9e9761490
Charan Teja <charan@nutanix.com>
2020-02-11 10:28:11 +0530

    Push kafka utils and example for api and consumers

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module sns
 go 1.12
 
 require (
+	github.com/gin-gonic/gin v1.5.0 // indirect
 	github.com/gorilla/mux v1.7.3
 	github.com/lib/pq v1.3.0
+	github.com/namsral/flag v1.7.4-pre // indirect
+	github.com/rs/zerolog v1.18.0 // indirect
+	github.com/segmentio/kafka-go v0.3.5
 	github.com/sirupsen/logrus v1.4.2
 )
---
+++ kafka/config.go
@@ -0,0 +1,28 @@
+package kafka
+
+import (
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+var writer *kafka.Writer
+
+func Configure(kafkaBrokerUrls []string, clientId string, topic string) (w *kafka.Writer, err error) {
+	dialer := &kafka.Dialer{
+		Timeout:  10 * time.Second,
+		ClientID: clientId,
+	}
+
+	config := kafka.WriterConfig{
+		Brokers:      kafkaBrokerUrls,
+		Topic:        topic,
+		Balancer:     &kafka.LeastBytes{},
+		Dialer:       dialer,
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  10 * time.Second,
+	}
+	w = kafka.NewWriter(config)
+	writer = w
+	return w, nil
+}
